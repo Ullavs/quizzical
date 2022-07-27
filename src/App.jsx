cbfd22d3f08281fed37e69896809cc181f575999
Ullavs <ullavansprang@gmail.com>
2022-07-27 16:25:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
-function App() {
-  return <div className="App">Quizzical</div>;
-}
+import QuestionForm from "./QuestionForm";
+import WelcomeScreen from "./WelcomeScreen";
 
-export default App;
+const questions = [
+  {
+    question: "How would one say goodbye in Spanish?",
+    options: ["Adios", "Hola", "Au Revoir", "Salir"],
+  },
+  {
+    question:
+      "Which best selling toy of 1983 caused hysteria, resulting in riots breaking in stores?",
+    options: [
+      "Cabbage Patch Kids",
+      "Transformers",
+      "Care Bears",
+      "Rubik's Cube",
+    ],
+  },
+  {
+    question: "Is Ulla altijd BOOS?",
+    options: ["Ja", "Absoluut", "Zeker weten", "Echt niet!"],
+  },
+  {
+    question: "Is Rob de beste tutor allertijden?",
+    options: ["Ja", "Absoluut", "Nee", "Misschien"],
+  },
+  {
+    question: "What is the hottest planet in our Solar System?",
+    options: ["Mercury", "Venus", "Mars", "Saturn"],
+  },
+];
+
+export default function App() {
+  return (
+    <main>
+      <QuestionForm questions={questions} />
+      {/* <WelcomeScreen /> */}
+    </main>
+  );
+}
