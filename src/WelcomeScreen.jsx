97f384ff940eb32662a84255ce2e3652af0d542e
Ullavs <ullavansprang@gmail.com>
2022-07-27 16:24:28 +0200

    Create WelcomeScreen.jsx

---
+++ src/WelcomeScreen.jsx
@@ -0,0 +1,9 @@
+export default function WelcomeScreen() {
+  return (
+    <div className="welcome-screen container">
+      <h1>Quizzical</h1>
+      <p>The most fun quiz ever</p>
+      <button>Start quiz</button>
+    </div>
+  );
+}
